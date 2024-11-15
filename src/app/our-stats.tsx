2b6cbbfe317e2f87294ffcf1437cc60dd4a676a4
aspiringcoder6 <127761097+aspiringcoder6@users.noreply.github.com>
2024-11-15 21:37:18 +0700

    final final commit

--- src/app/our-stats.tsx
+++ src/app/our-stats.tsx
@@ -9,10 +9,6 @@ const STATS = [
     title: "Người tham gia",
   },
   {
-    count: "3",
-    title: "Điểm rèn luyện",
-  },
-  {
     count: "13",
     title: "Đơn vị",
   },
@@ -26,20 +22,35 @@ export function OurStats() {
   return (
     <section className="container mx-auto grid gap-10 px-8 py-44 lg:grid-cols-1 lg:gap-20 xl:grid-cols-2 xl:place-items-center">
       <div>
-        <Typography variant="h6" color="orange" className="mb-6 font-medium" placeholder=""onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
+        <Typography
+          variant="h6"
+          color="orange"
+          className="mb-6 font-medium"
+          placeholder=""
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        >
           Sự kiện của chúng tôi
         </Typography>
         <Typography
           className="text-5xl font-bold leading-tight lg:w-3/4"
-          color="blue-gray" placeholder=""onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}
+          color="blue-gray"
+          placeholder=""
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
         >
           Điểm nhấn của Cuộc thi Lights of HUST
         </Typography>
         <Typography
           variant="lead"
-          className="mt-3 w-full !text-gray-500 lg:w-9/12" placeholder=""onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}
+          className="mt-3 w-full !text-gray-500 lg:w-9/12"
+          placeholder=""
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
         >
-          Sự kiện đặc biệt này khuyến khích sinh viên chia sẻ những câu chuyện truyền cảm hứng, trải nghiệm học tập và khoảnh khắc đáng nhớ trong đời sống sinh viên tại Đại học Bách khoa Hà Nội.
+          Sự kiện đặc biệt này khuyến khích sinh viên chia sẻ những câu chuyện
+          truyền cảm hứng, trải nghiệm học tập và khoảnh khắc đáng nhớ trong đời
+          sống sinh viên tại Đại học Bách khoa Hà Nội.
         </Typography>
       </div>
       <div>
